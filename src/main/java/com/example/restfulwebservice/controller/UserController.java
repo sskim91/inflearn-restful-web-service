33dbd8e13fd60c907766998dd20cb8819ff5dd1b
sskim91 <tjdtjq91@gmail.com>
2020-10-13 22:16:59 +0900

    feat: 사용자 등록 HTTP POST, POSTMAN body에 json 값 넣어서 테스트해보기

--- src/main/java/com/example/restfulwebservice/controller/UserController.java
+++ src/main/java/com/example/restfulwebservice/controller/UserController.java
@@ -2,9 +2,7 @@ package com.example.restfulwebservice.controller;
 
 import com.example.restfulwebservice.domain.User;
 import com.example.restfulwebservice.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +27,10 @@ public class UserController {
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {    //
         return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
     }
 }
