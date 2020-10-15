49414221f03ec3a310dd04c601acb25a0a44aee4
sskim91 <tjdtjq91@gmail.com>
2020-10-15 23:39:41 +0900

    feat: URI를 이용한 REST API Version 관리

--- src/main/java/com/example/restfulwebservice/controller/AdminUserController.java
+++ src/main/java/com/example/restfulwebservice/controller/AdminUserController.java
@@ -1,11 +1,13 @@
 package com.example.restfulwebservice.controller;
 
 import com.example.restfulwebservice.domain.User;
+import com.example.restfulwebservice.domain.UserV2;
 import com.example.restfulwebservice.exception.UserNotFoundException;
 import com.example.restfulwebservice.service.UserService;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -43,9 +45,9 @@ public class AdminUserController {
     }
 
     //사용자 한명
-    //GET /users/1 or /users/10 -> int형으로 보내도 String형으로 변환됨. but 이런 문자형태도 int 형으로 선언하면 자동으로 converting 된다.
-    @GetMapping("/users/{id}")
-    public MappingJacksonValue retrieveUser(@PathVariable int id) {
+    //GET admin/users/1 -> /admin/v1/users/1
+    @GetMapping("/v1/users/{id}")
+    public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
 
         User user = service.findOne(id);
         if (user == null) {
@@ -60,6 +62,32 @@ public class AdminUserController {
         FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
 
         MappingJacksonValue mapping = new MappingJacksonValue(user);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    @GetMapping("/v2/users/{id}")
+    public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
+
+        User user = service.findOne(id);
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        // User -> User2
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(user, userV2); // id, name, joinDate, password, ssn
+        userV2.setGrade("VIP");
+
+        //프로그램 내부에서 다양한 속성을 제어할 수 있다는 장점이 있다.
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "grade");
+
+        //필터를 추가할 때는 빈의 이름을 적어줘야 하는데 domain의 @JsonFilter에 지정한 이름이 id이다.
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfoV2", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(userV2);
         mapping.setFilters(filters);
 
         return mapping;
