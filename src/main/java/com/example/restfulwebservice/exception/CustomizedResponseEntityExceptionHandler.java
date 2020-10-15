c149d0435dec956a66266d7ae30236ea224bc050
sskim91 <tjdtjq91@gmail.com>
2020-10-15 22:11:05 +0900

    feat: 유효성 체크를 위한 Validation API 사용
    @Size, @Past 어노테이션이 boot 2.3.4 릴리즈에서는 안되서 찾아봐야함..
    
    강의에 맞게 버전을 2.1.17로 낮춤 강의는 (2.1.13)

--- src/main/java/com/example/restfulwebservice/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/example/restfulwebservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.example.restfulwebservice.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +32,17 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 
         //return 되는 내용은 생성시켰던 ExceptionResponse 객체의 내용
         return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),
+                "Validation Failed",
+                ex.getBindingResult().toString());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
