7a5eb22237bdff614e338d3692ca7795f92d22a0
sskim91 <tjdtjq91@gmail.com>
2020-10-15 22:22:36 +0900

    feat: 다국어 처리를 위한 Internationalized 구현 방법

--- src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
+++ src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
@@ -2,6 +2,11 @@ package com.example.restfulwebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServiceApplication {
@@ -10,4 +15,13 @@ public class RestfulWebServiceApplication {
         SpringApplication.run(RestfulWebServiceApplication.class, args);
     }
 
+    /**
+     * 스프링 부트가 초기화 될때 스프링 컨테이너에 올라가서 사용할 수 있다.
+     */
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREA);
+        return localeResolver;
+    }
 }
--- src/main/java/com/example/restfulwebservice/controller/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/controller/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.example.restfulwebservice.controller;
 
 import com.example.restfulwebservice.domain.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    //메시지 프로퍼티 받을 때 사용
+    @Autowired
+    private MessageSource messageSource;
+
     //GET
     // /hello-world (endpoint) 사용자에 의해서 호출되는 uri를 endpoint라고 한다.
 //    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
@@ -24,5 +34,12 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldBean(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path ="/hello-world-internationalized")
+    public String helloWorldInternationalized(
+            @RequestHeader(name="Accept-Language", required = false) Locale locale) {
+
+        return messageSource.getMessage("greeting.message", null, locale);
     }
 }
