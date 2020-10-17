5e039fff87809012156b38dd1443515770fc4eb6
sskim91 <tjdtjq91@gmail.com>
2020-10-17 21:07:54 +0900

    feat: REST API Documentation을 위한 Swagger 사용 Swagger Documentation 구현 방법

--- src/main/java/com/example/restfulwebservice/domain/User.java
+++ src/main/java/com/example/restfulwebservice/domain/User.java
@@ -1,5 +1,7 @@
 package com.example.restfulwebservice.domain;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,16 +15,21 @@ import java.util.Date;
 @NoArgsConstructor
 //@JsonIgnoreProperties(value = {"password"})  //클래스 블록에서 사용하는방법
 //@JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
     private Integer id;
 
     @Size(min = 2, message = "Name은 2글자 이상 입력해주세요.")    //2자 이상
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     private String name;
     @Past   //과거입력 안됨
+    @ApiModelProperty(notes = "사용자 등록일을 입력해 주세요.")
     private Date joinDate;
 
     //외부에 노출시키고 싶지 않은 데이터 제어하기
 //    @JsonIgnore
+    @ApiModelProperty(notes = "사용자 패스워드를 입력해 주세요.")
     private String password;
+    @ApiModelProperty(notes = "사용자 주민번호를 입력해 주세요.")
     private String ssn; //주민등록번호
 }
